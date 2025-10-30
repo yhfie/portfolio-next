b4367430a965af684338a3e411924c7dc5ee2540
yhfie <huwaidar5@gmail.com>
2025-10-30 21:22:44 +0700

    fix: fix build

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,18 +2,4 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-module.exports = {
-  darkMode: "class",
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"],
-    base: false,
-    darkTheme: "dark",
-  },
-};
-
-
 export default config;
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-      darkMode: "class",
-      content: [
-            "./app/**/*.{js,ts,jsx,tsx}",
-            "./components/**/*.{js,ts,jsx,tsx}",
-      ],
-}
